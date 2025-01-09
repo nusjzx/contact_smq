27a8e9a7c8d87ebca48124adcf1c145ff736887e
Jia Zhixin <zhixin.jia@shopee.com>
2025-01-09 23:15:05 +0800

    first commit

---
+++ curl.sh
@@ -0,0 +1,9 @@
+curl --location --request POST 'https://space.shopee.io/apis/cmdb/v2/service/container/restart' \
+--header 'sec-ch-ua-platform: "macOS"' \
+--header 'authorization: Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImI3OTBlYjE5MmY2YTJkMTZjNDQwZWE3NGRlODU4ODg1NDc5NjZmZDEiLCJ0eXAiOiJKV1QifQ.eyJlbWFpbCI6InpoaXhpbi5qaWFAc2hvcGVlLmNvbSIsImV4cCI6MTczNjc1MjU0OSwiaXNzIjoiaHR0cHM6Ly9zcGFjZS5zaG9wZWUuaW8vdjEvY2VydHMiLCJzdWIiOiI0MDAyMjkifQ.TaNu1eF_ZkxSU1sIyULWBIeEUN7YekOjPelgc3pBSNO_Yn_2e8kxx0HSgSDaiX0_TKVyJahtmjX7lIRF5b3CUJB-VncnWJMq689Cw-TldU76YcDj62P0WrLfEgjsh5Sa7MUczbZElzYyZhXVcgMqJYETEMMrqYbUhlGLiFEzLNzot9FJLYnf60B9gOdcgJKv9x_EgUoc6nAQT8W6NbI2uu19T4hw-ZOZlNYP1mBKebZBY_1OwTtcLPhslfzQ3nDwyptii3At8ld56C2Oac_c_jJ5-rpQ71Ueo0fNV5KVYJ1hpHNKQOQdaUQHct-WIUfdo5I1eF8RVm_f-6Q0vuO_d0OY63JgsvvRZndCR-eX0Ct6FtWj34ZiIMDKMtkq9KkcyvVDfzA3WKyTgjB1cq8F9SWP_sJJqgh72AWecvzl7qnWmQKjLxiwIWJKCFNDDyTtOKdadkiwQDC0xJiWbTfVT1Vf2Wqp-AzH6dMbYv4FSQdt5k4ALghxoHzOrdxJOxYv6l-4yW4lpLk8vgANJjg18fWDQemsOSgwCVNor2AG-0QdFVMhkSpm6Iuoj9Av23H819bM2toKKG0od_ygdvL6V28-LX4Z7AQpjQScEfXtBO7XW65D6loirMvEcE8SdEEi5fjkQQ14bD1BoEnLqPnYdXykKttQCDYqeeTiRiJwdHM' \
+--header 'Referer: https://space.shopee.io/console/cmdb/compute/detail/shopee.marketplace_core.buyer.user.identity.contact.contactsmq/container' \
+--header 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' \
+--header 'sec-ch-ua: "Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"' \
+--header 'content-type: application/json' \
+--header 'sec-ch-ua-mobile: ?0' \
+--data-raw '{"resource_type":"CONTAINER","sdu":"account-contactsmq-live-global","env":"live","idc":"sg7"}'
